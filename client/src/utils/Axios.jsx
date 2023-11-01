221eac394a45e973562447cd893cc23bee87523c
Priyanshu1112 <97700245+Priyanshu1112@users.noreply.github.com>
2023-11-02 02:42:39 +0530

    Update Axios.jsx

--- client/src/utils/Axios.jsx
+++ client/src/utils/Axios.jsx
@@ -4,6 +4,10 @@ const instance = axios.create({
   baseURL: "https://stock-whisperer-api.onrender.com/",
   // baseURL: "http://localhost:3000",
   withCredentials: true,
+  headers: {
+    // Add default headers here
+    'Content-Type': 'application/json', // Example content type
+  },
 });
 
 export default instance;
