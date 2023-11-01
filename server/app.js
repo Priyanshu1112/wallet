0544b8a7ccf5cfbd0c87ef8761768ae2fc95d2b4
Priyanshu1112 <97700245+Priyanshu1112@users.noreply.github.com>
2023-11-01 23:03:30 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -7,12 +7,28 @@ const cors = require("cors");
 require("./models/database").connectDatabase();
 
 //cors
-app.use(
-  cors({
-    origin: ["http://localhost:5173", "https://stock-whisperer.onrender.com"],
-    // credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: ["http://localhost:5173", "https://stock-whisperer.onrender.com"],
+//     // credentials: true,
+//   })
+// );
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.header("Access-Control-Allow-Origin", "https://stock-whisperer.onrender.com");
+  res.header("Access-Control-Allow-Credentials", "true");
+  // You can also set other CORS headers as needed, such as methods and headers.
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+  if (req.method === "OPTIONS") {
+    // Handle preflight requests for CORS
+    res.sendStatus(204); // No content, just indicate that CORS is allowed
+  } else {
+    // Continue with the next middleware or route handler
+    next();
+  }
+});
 
 //logger
 const logger = require("morgan");
