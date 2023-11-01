cd543ffbfec594d33586b71e308e65a921de6695
Priyanshu1112 <ashu1122sing@gmail.com>
2023-11-02 02:20:02 +0530

    update

--- client/src/utils/Axios.jsx
+++ client/src/utils/Axios.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://stock-whisperer-api.onrender.com",
+  baseURL: "https://stock-whisperer-api.onrender.com/",
   // baseURL: "http://localhost:3000",
   withCredentials: true,
 });
