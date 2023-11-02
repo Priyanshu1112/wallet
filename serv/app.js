1b12d68b2698e802781ccac14af421350b739d89
Priyanshu1112 <ashu1122sing@gmail.com>
2023-11-02 13:10:07 +0530

    errorHandling

--- serv/app.js
+++ serv/app.js
@@ -32,19 +32,13 @@ app.use("/admin", adminRouter);
 app.use("/user", usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
-});
 
 // error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
+const ErrorHandler = require("./utils/ErrorHandler");
+const { generatedErrors } = require("./middlewares/error");
+app.use((req, res, next) => {
+  next(new ErrorHandler(`Request URL Not Found: ${req.url}`), 404);
 });
+app.use(generatedErrors);
 
 module.exports = app;
